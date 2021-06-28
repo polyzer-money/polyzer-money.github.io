c0053d89beb1b2802fa78998f870c48eef70631a
polyzer-money <lolesportsplayer@gmail.com>
2021-06-28 01:01:57 -0400

    update

--- js/script.js
+++ js/script.js
@@ -8,15 +8,19 @@ switch(imgType){
     case "1":
         document.getElementById("c").remove();
         document.getElementById("i").src = "https://i.imgur.com/2bwE3Ik.png";
+	/*
         document.getElementsByTagName('meta')[2].content = "https://polyzer-money.github.io?img=1";
         document.getElementsByTagName('meta')[3].content = "https://i.imgur.com/2bwE3Ik.png";
+	*/
         break;
     default:
-        document.getElementById("i").remove();
+	document.getElementById("i").remove();
+	/*
         document.getElementsByTagName('meta')[0].content = "Polyzer's Website"; //title
         document.getElementsByTagName('meta')[1].content = "Website where I test fun stuff"; //description
         document.getElementsByTagName('meta')[2].content = "https://polyzer-money.github.io"; //url
         document.getElementsByTagName('meta')[3].content = "https://i.imgur.com/yFq4cSC.png"; //image
+	*/
         break;
 }
 
