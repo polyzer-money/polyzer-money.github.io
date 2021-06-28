5c2c902cd561de6a4f7993da40ec87d2da02e41f
polyzer-money <lolesportsplayer@gmail.com>
2021-06-28 00:33:02 -0400

    more testing

--- js/script.js
+++ js/script.js
@@ -7,12 +7,17 @@ const imgType = urlParams.get('img')
 switch(imgType){
     case "1":
         document.getElementById("c").remove();
-        document.getElementById("i").src = "https://i.imgur.com/yFq4cSC.png";
+        document.getElementById("i").src = "https://i.imgur.com/2bwE3Ik.png";
         document.getElementsByTagName('meta')["og:url"].content = `https://polyzer-money.github.io?img=${imgType}`;
         document.getElementsByTagName('meta')["og:image"].content = "https://i.imgur.com/2bwE3Ik.png";
         break;
     default:
         document.getElementById("i").remove();
+        document.getElementsByTagName('meta')["og:type"].content = "website";
+        document.getElementsByTagName('meta')["og:title"].content = "Polyzer's Website";
+        document.getElementsByTagName('meta')["og:description"].content = "Website where I test fun stuff";
+        document.getElementsByTagName('meta')["og:url"].content = "https://polyzer-money.github.io";
+        document.getElementsByTagName('meta')["og:image"].content = "https://i.imgur.com/yFq4cSC.png";
         break;
 }
 
